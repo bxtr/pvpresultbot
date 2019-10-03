2fa8329f3f5132765e13432807c891920b8a5cce
Богачев Дмитрий Александрович <bogachev-da@it-energy.ru>
2019-10-03 17:03:09 +0300

    added to player new attributes (friend code, in game nickname, home town name).

--- src/main/databasescript/tables.sql
+++ src/main/databasescript/tables.sql
@@ -3,7 +3,10 @@
 
 CREATE TABLE player (
     id serial PRIMARY KEY,
-    player_name VARCHAR(200)
+    player_name VARCHAR(200),
+    friend_code VARCHAR(200),
+    in_game_nickname VARCHAR(200),
+    town_name VARCHAR(200)
 );
 
 CREATE TABLE fight_result (
--- src/main/java/org/bxtr/PvpBot/model/Player.java
+++ src/main/java/org/bxtr/PvpBot/model/Player.java
@@ -2,17 +2,44 @@ package org.bxtr.PvpBot.model;
 
 import javax.persistence.*;
 
+/**
+ * Модель для игрока.
+ */
 @Entity
 @Table(name = "PLAYER")
 public class Player {
 
+    /**
+     * Идентификатор
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private Long id;
 
+    /**
+     * Никнейм в телеграмме
+     */
     @Column(name = "PLAYER_NAME")
     private String name;
+
+    /**
+     * Код для добавления в друзья из PoGO
+     */
+    @Column(name = "FRIEND_CODE")
+    private String friendCode;
+
+    /**
+     * Ник в игре
+     */
+    @Column(name = "IN_GAME_NICKNAME")
+    private String inGameNickName;
+
+    /**
+     * Название города из которого игрок
+     */
+    @Column(name = "TOWN_NAME")
+    private String townName;
 
     public Player() {
         //empty
@@ -33,6 +60,33 @@ public class Player {
 
     public Player setName(String name) {
         this.name = name;
+        return this;
+    }
+
+    public String getFriendCode() {
+        return friendCode;
+    }
+
+    public Player setFriendCode(String friendCode) {
+        this.friendCode = friendCode;
+        return this;
+    }
+
+    public String getInGameNickName() {
+        return inGameNickName;
+    }
+
+    public Player setInGameNickName(String inGameNickName) {
+        this.inGameNickName = inGameNickName;
+        return this;
+    }
+
+    public String getTownName() {
+        return townName;
+    }
+
+    public Player setTownName(String townName) {
+        this.townName = townName;
         return this;
     }
 }
