e829e5ddff1858ec66268236dce7f2145476b776
Богачев Дмитрий Александрович <bogachev-da@it-energy.ru>
2019-10-08 12:41:43 +0300

    fix validation text

--- src/main/java/org/bxtr/PvpBot/commands/AddFightResultCommand.java
+++ src/main/java/org/bxtr/PvpBot/commands/AddFightResultCommand.java
@@ -48,7 +48,7 @@ public class AddFightResultCommand extends BotCommand {
             sendMessage.setText("Результат добавлен");
         } else {
             final StringBuilder stringBuilder = new StringBuilder();
-            errrors.forEach(item -> stringBuilder.append(item).append(";\n"));
+            errrors.forEach(item -> stringBuilder.append(item).append(";\n\n"));
             sendMessage.setText(stringBuilder.toString());
         }
 
@@ -69,9 +69,9 @@ public class AddFightResultCommand extends BotCommand {
                 int scorePlayer1 = Integer.parseInt(arguments[2]);
                 int scorePlayer2 = Integer.parseInt(arguments[3]);
                 if (!checkPlayer(player1.toLowerCase()))
-                    errors.add("первый игрок не найден. возможно не правильно написан ник или он не заргестрирован.");
+                    errors.add("первый игрок не найден. возможно, не правильно написан ник или он не зарегестрирован");
                 if (!checkPlayer(player2.toLowerCase()))
-                    errors.add("второй игрок не найден. возможно не правильно написан ник или он не заргестрирован.");
+                    errors.add("второй игрок не найден. возможно, не правильно написан ник или он не зарегестрирован");
                 if (!checkScore(scorePlayer1))
                     errors.add("счет для первого игрока неправильно указан, допустимо указывать значение от 0 до 3");
                 if (!checkScore(scorePlayer2))
@@ -81,7 +81,7 @@ public class AddFightResultCommand extends BotCommand {
                 if (!((scorePlayer1 + scorePlayer2) >= 2))
                     errors.add("сумма результатов игроков должна быть больше 1");
                 if (scorePlayer1 == scorePlayer2)
-                    errors.add("у игроков должен быть различное количество выигранных раундов");
+                    errors.add("у игроков должно быть различное количество выигранных раундов");
 
             } catch (Exception e) {
                 errors.add("неверный формат переменных, возможно, ник и счет стоят не на своих местах");
